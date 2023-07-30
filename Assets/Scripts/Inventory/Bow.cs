faf5d4d23ee01670b13deb3ef80d97a853edf3bf
Pakanun <dear_so17@hotmail.com>
2023-07-30 23:28:04 +0700

    bow & arrow

--- Assets/Scripts/Inventory/Bow.cs
+++ Assets/Scripts/Inventory/Bow.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Bow : MonoBehaviour, IWeapon
 {
     [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform arrowSpawnPoint;
+
+    private Animator anim;
+
+    readonly int FIRE_HASH = Animator.StringToHash("Fire");
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     public void Attack()
     {
-        Debug.Log("Bow Attack");
+        anim.SetTrigger(FIRE_HASH);
+        GameObject newArrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, ActiveWeapon.Instance.transform.rotation);
     }
 
     public WeaponInfo GetWeaponInfo()

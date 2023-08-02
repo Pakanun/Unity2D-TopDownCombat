00600b3961449ae42d720e2d813fe176017b1457
Pakanun <dear_so17@hotmail.com>
2023-08-02 23:07:49 +0700

    magic staff

--- Assets/Scripts/Inventory/Projectile.cs
+++ Assets/Scripts/Inventory/Projectile.cs
@@ -34,7 +34,6 @@ public class Projectile : MonoBehaviour
 
         if(!collision.isTrigger && (enemyHealth || indestructible))
         {
-            enemyHealth?.TakeDamage(weaponInfo.weaponDamage);
             Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
             Destroy(gameObject);
 
